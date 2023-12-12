852c0ea38d6ea66fc9fc2c8ef549a55e19b8ab62
Luis Maitret <maitret@myhostmx.com>
2023-12-12 15:49:08 -0600

    12/12/2023 + Tronix

---
+++ file/themes-assets/tronix/assets/js/main.js
@@ -0,0 +1,549 @@
+(function ($) {
+    "use strict";
+    /*=================================
+        JS Index Here
+    ==================================*/
+    /*
+    01. On Load Function
+    02. Preloader
+    03. Mobile Menu
+    04. Sticky fix
+    05. Scroll To Top
+    06. Set Background Image Color & Mask
+    07. Global Slider
+    08. Ajax Contact Form
+    09. Popup Sidemenu
+    10. Magnific Popup
+    11. Filter
+    12. Counter Up
+    13. Shape Mockup
+    14. Progress Bar Animation
+    00. Right Click Disable
+    */
+    /*=================================
+        JS Index End
+    ==================================*/
+    /*
+
+  /*---------- 01. On Load Function ----------*/
+    $(window).on("load", function () {
+        $(".preloader").fadeOut();
+    });
+
+    /*---------- 02. Preloader ----------*/
+    if ($(".preloader").length > 0) {
+        $(".preloaderCls").each(function () {
+            $(this).on("click", function (e) {
+                e.preventDefault();
+                $(".preloader").css("display", "none");
+            });
+        });
+    }
+
+    /*---------- 03. Mobile Menu ----------*/
+    $.fn.otmobilemenu = function (options) {
+        var opt = $.extend(
+            {
+                menuToggleBtn: ".ot-menu-toggle",
+                bodyToggleClass: "ot-body-visible",
+                subMenuClass: "ot-submenu",
+                subMenuParent: "ot-item-has-children",
+                subMenuParentToggle: "ot-active",
+                meanExpandClass: "ot-mean-expand",
+                appendElement: '<span class="ot-mean-expand"></span>',
+                subMenuToggleClass: "ot-open",
+                toggleSpeed: 400,
+            },
+            options
+        );
+
+        return this.each(function () {
+            var menu = $(this); // Select menu
+
+            // Menu Show & Hide
+            function menuToggle() {
+                menu.toggleClass(opt.bodyToggleClass);
+
+                // collapse submenu on menu hide or show
+                var subMenu = "." + opt.subMenuClass;
+                $(subMenu).each(function () {
+                    if ($(this).hasClass(opt.subMenuToggleClass)) {
+                        $(this).removeClass(opt.subMenuToggleClass);
+                        $(this).css("display", "none");
+                        $(this).parent().removeClass(opt.subMenuParentToggle);
+                    }
+                });
+            }
+
+            // Class Set Up for every submenu
+            menu.find("li").each(function () {
+                var submenu = $(this).find("ul");
+                submenu.addClass(opt.subMenuClass);
+                submenu.css("display", "none");
+                submenu.parent().addClass(opt.subMenuParent);
+                submenu.prev("a").append(opt.appendElement);
+                submenu.next("a").append(opt.appendElement);
+            });
+
+            // Toggle Submenu
+            function toggleDropDown($element) {
+                if ($($element).next("ul").length > 0) {
+                    $($element).parent().toggleClass(opt.subMenuParentToggle);
+                    $($element).next("ul").slideToggle(opt.toggleSpeed);
+                    $($element).next("ul").toggleClass(opt.subMenuToggleClass);
+                } else if ($($element).prev("ul").length > 0) {
+                    $($element).parent().toggleClass(opt.subMenuParentToggle);
+                    $($element).prev("ul").slideToggle(opt.toggleSpeed);
+                    $($element).prev("ul").toggleClass(opt.subMenuToggleClass);
+                }
+            }
+
+            // Submenu toggle Button
+            var expandToggler = "." + opt.meanExpandClass;
+            $(expandToggler).each(function () {
+                $(this).on("click", function (e) {
+                    e.preventDefault();
+                    toggleDropDown($(this).parent());
+                });
+            });
+
+            // Menu Show & Hide On Toggle Btn click
+            $(opt.menuToggleBtn).each(function () {
+                $(this).on("click", function () {
+                    menuToggle();
+                });
+            });
+
+            // Hide Menu On out side click
+            menu.on("click", function (e) {
+                e.stopPropagation();
+                menuToggle();
+            });
+
+            // Stop Hide full menu on menu click
+            menu.find("div").on("click", function (e) {
+                e.stopPropagation();
+            });
+        });
+    };
+
+    $(".ot-menu-wrapper").otmobilemenu();
+
+    /*---------- 04. Sticky fix ----------*/
+    $(window).scroll(function () {
+        var topPos = $(this).scrollTop();
+        if (topPos > 500) {
+            $('.sticky-wrapper').addClass('sticky');
+            $('.category-menu').addClass('close-category');
+        } else {
+            $('.sticky-wrapper').removeClass('sticky')
+            $('.category-menu').removeClass('close-category');
+        }
+    })
+
+    /*---------- 05. Scroll To Top ----------*/
+    if ($('.scroll-top').length > 0) {
+        
+        var scrollTopbtn = document.querySelector('.scroll-top');
+        var progressPath = document.querySelector('.scroll-top path');
+        var pathLength = progressPath.getTotalLength();
+        progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
+        progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
+        progressPath.style.strokeDashoffset = pathLength;
+        progressPath.getBoundingClientRect();
+        progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';		
+        var updateProgress = function () {
+            var scroll = $(window).scrollTop();
+            var height = $(document).height() - $(window).height();
+            var progress = pathLength - (scroll * pathLength / height);
+            progressPath.style.strokeDashoffset = progress;
+        }
+        updateProgress();
+        $(window).scroll(updateProgress);	
+        var offset = 50;
+        var duration = 750;
+        jQuery(window).on('scroll', function() {
+            if (jQuery(this).scrollTop() > offset) {
+                jQuery(scrollTopbtn).addClass('show');
+            } else {
+                jQuery(scrollTopbtn).removeClass('show');
+            }
+        });				
+        jQuery(scrollTopbtn).on('click', function(event) {
+            event.preventDefault();
+            jQuery('html, body').animate({scrollTop: 0}, duration);
+            return false;
+        })
+    }
+
+    /*---------- 06. Set Background Image Color & Mask ----------*/
+    if ($("[data-bg-src]").length > 0) {
+        $("[data-bg-src]").each(function () {
+            var src = $(this).attr("data-bg-src");
+            $(this).css("background-image", "url(" + src + ")");
+            $(this).removeAttr("data-bg-src").addClass("background-image");
+        });
+    }
+
+    if ($('[data-bg-color]').length > 0) {
+        $('[data-bg-color]').each(function () {
+          var color = $(this).attr('data-bg-color');
+          $(this).css('background-color', color);
+          $(this).removeAttr('data-bg-color');
+        });
+    };
+      
+    if ($('[data-mask-src]').length > 0) {
+        $('[data-mask-src]').each(function () {
+          var mask = $(this).attr('data-mask-src');
+          $(this).css({
+            'mask-image': 'url(' + mask + ')',
+            '-webkit-mask-image': 'url(' + mask + ')'
+          });
+          $(this).addClass('bg-mask');
+          $(this).removeAttr('data-mask-src');
+        });
+    };
+
+    /*----------- 07. Global Slider ----------*/
+
+    $('.ot-slider').each(function () {
+
+        var otSlider = $(this);
+        var settings = $(this).data('slider-options');
+
+        // Store references to the navigation Slider
+        var prevArrow = otSlider.find('.slider-prev');
+        var nextArrow = otSlider.find('.slider-next');
+        var paginationEl = otSlider.find('.slider-pagination');
+
+        var autoplayconditon = settings['autoplay'];
+
+        var sliderDefault = {
+            slidesPerView: settings['slidesPerView'] ? settings['slidesPerView'] : 1,
+            spaceBetween: settings['spaceBetween'] ? settings['spaceBetween'] : 24,
+            loop: settings['loop'] == false ? false : true,
+            speed: settings['speed'] ? settings['speed'] : 1000,
+            autoplay: autoplayconditon ? autoplayconditon : {delay: 6000, disableOnInteraction: false},
+            navigation: {
+                nextEl: nextArrow.get(0),
+                prevEl: prevArrow.get(0),  
+            },
+            pagination: {
+                el: paginationEl.get(0),
+                clickable: true, 
+                renderBullet: function (index, className) {
+                    return '<span class="' + className + '" aria-label="Go to Slide ' + (index + 1) + '"></span>';
+                },
+            },
+        };
+
+        var options = JSON.parse(otSlider.attr('data-slider-options'));
+        options = $.extend({}, sliderDefault, options);
+        var swiper = new Swiper(otSlider.get(0), options); // Assign the swiper variable
+
+        if ($('.slider-area').length > 0) {
+            $('.slider-area').closest(".container").parent().addClass("arrow-wrap");
+        }
+
+    });
+
+    // Function to add animation classes
+    function animationProperties() {
+        $('[data-ani]').each(function () {
+            var animationName = $(this).data('ani');
+            $(this).addClass(animationName);
+        });
+
+        $('[data-ani-delay]').each(function () {
+            var delayTime = $(this).data('ani-delay');
+            $(this).css('animation-delay', delayTime);
+        });
+    }
+    animationProperties();
+
+    // Add click event handlers for external slider arrows based on data attributes
+    $('[data-slider-prev], [data-slider-next]').on('click', function () {
+        var sliderSelector = $(this).data('slider-prev') || $(this).data('slider-next');
+        var targetSlider = $(sliderSelector);
+
+        if (targetSlider.length) {
+            var swiper = targetSlider[0].swiper;
+
+            if (swiper) {
+                if ($(this).data('slider-prev')) {
+                    swiper.slidePrev(); 
+                } else {
+                    swiper.slideNext(); 
+                }
+            }
+        }
+    });
+
+    let SwiperTicker = new Swiper('.swiper-newsticker', {
+        spaceBetween: 130,
+        slidesPerView: 'auto',
+        speed: 4000,
+        autoplay: {
+          delay: 2,
+          disableOnInteraction: false,
+        //   pauseOnMouseEnter: true,
+        },
+        loop: true,
+        allowTouchMove: false,
+        disableOnInteraction: true,
+    });
+
+    /*----------- 08. Ajax Contact Form ----------*/
+    var form = ".ajax-contact";
+    var invalidCls = "is-invalid";
+    var $email = '[name="email"]';
+    var $validation =
+        '[name="name"],[name="email"],[name="subject"],[name="number"],[name="message"]'; // Must be use (,) without any space
+    var formMessages = $(".form-messages");
+
+    function sendContact() {
+        var formData = $(form).serialize();
+        var valid;
+        valid = validateContact();
+        if (valid) {
+            jQuery
+                .ajax({
+                    url: $(form).attr("action"),
+                    data: formData,
+                    type: "POST",
+                })
+                .done(function (response) {
+                    // Make sure that the formMessages div has the 'success' class.
+                    formMessages.removeClass("error");
+                    formMessages.addClass("success");
+                    // Set the message text.
+                    formMessages.text(response);
+                    // Clear the form.
+                    $(
+                        form +
+                            ' input:not([type="submit"]),' +
+                            form +
+                            " textarea"
+                    ).val("");
+                })
+                .fail(function (data) {
+                    // Make sure that the formMessages div has the 'error' class.
+                    formMessages.removeClass("success");
+                    formMessages.addClass("error");
+                    // Set the message text.
+                    if (data.responseText !== "") {
+                        formMessages.html(data.responseText);
+                    } else {
+                        formMessages.html(
+                            "Oops! An error occured and your message could not be sent."
+                        );
+                    }
+                });
+        }
+    }
+
+    function validateContact() {
+        var valid = true;
+        var formInput;
+
+        function unvalid($validation) {
+            $validation = $validation.split(",");
+            for (var i = 0; i < $validation.length; i++) {
+                formInput = form + " " + $validation[i];
+                if (!$(formInput).val()) {
+                    $(formInput).addClass(invalidCls);
+                    valid = false;
+                } else {
+                    $(formInput).removeClass(invalidCls);
+                    valid = true;
+                }
+            }
+        }
+        unvalid($validation);
+
+        if (
+            !$($email).val() ||
+            !$($email)
+                .val()
+                .match(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/)
+        ) {
+            $($email).addClass(invalidCls);
+            valid = false;
+        } else {
+            $($email).removeClass(invalidCls);
+            valid = true;
+        }
+        return valid;
+    }
+
+    $(form).on("submit", function (element) {
+        element.preventDefault();
+        sendContact();
+    });
+
+    /*---------- 09. Popup Sidemenu ----------*/
+    function popupSideMenu($sideMenu, $sideMunuOpen, $sideMenuCls, $toggleCls) {
+        // Sidebar Popup
+        $($sideMunuOpen).on("click", function (e) {
+            e.preventDefault();
+            $($sideMenu).addClass($toggleCls);
+        });
+        $($sideMenu).on("click", function (e) {
+            e.stopPropagation();
+            $($sideMenu).removeClass($toggleCls);
+        });
+        var sideMenuChild = $sideMenu + " > div";
+        $(sideMenuChild).on("click", function (e) {
+            e.stopPropagation();
+            $($sideMenu).addClass($toggleCls);
+        });
+        $($sideMenuCls).on("click", function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+            $($sideMenu).removeClass($toggleCls);
+        });
+    }
+    popupSideMenu(".sidemenu-info",".sideMenuInfo",".sideMenuCls","show");
+    // popupSideMenu(".sidemenu-cart",".sideMenuCart",".sideMenuCls","show");
+    // popupSideMenu( ".popup-search-box", ".searchBoxToggler", ".searchClose", "show" );
+
+    /*----------- 10. Magnific Popup ----------*/
+    /* magnificPopup img view */
+    $(".popup-image").magnificPopup({
+        type: "image",
+        mainClass: 'mfp-zoom-in', 
+        removalDelay: 260,
+        gallery: {
+            enabled: true,
+        },
+    });
+
+    /* magnificPopup video view */
+    $(".popup-video").magnificPopup({
+        type: "iframe",
+        mainClass: 'mfp-zoom-in', 
+        removalDelay: 260,
+    });
+
+    /* magnificPopup video view */
+    $(".popup-content").magnificPopup({
+        type: "inline",
+        midClick: true,
+        mainClass: 'mfp-zoom-in', 
+        removalDelay: 260,
+    });
+
+    /*----------- 11. Filter ----------*/
+    $(".filter-active").imagesLoaded(function () {
+        var $filter = ".filter-active",
+            $filterItem = ".filter-item",
+            $filterMenu = ".filter-menu-active";
+
+        if ($($filter).length > 0) {
+            var $grid = $($filter).isotope({
+                itemSelector: $filterItem,
+                filter: "*",
+                masonry: {
+                    columnWidth: 1,
+                },
+            });
+
+            // filter items on button click
+            $($filterMenu).on("click", "button", function () {
+                var filterValue = $(this).attr("data-filter");
+                $grid.isotope({
+                    filter: filterValue,
+                });
+            });
+
+            // Menu Active Class
+            $($filterMenu).on("click", "button", function (event) {
+                event.preventDefault();
+                $(this).addClass("active");
+                $(this).siblings(".active").removeClass("active");
+            });
+        }
+    });
+
+    /*----------- 12. Counter Up ----------*/
+    $(".counter-number").counterUp({
+        delay: 10,
+        time: 1000,
+    });
+    $(".counter-number2").counterUp({
+        delay: 25,
+        time: 1600,
+    });
+
+    /*----------- 13. Shape Mockup ----------*/
+    $.fn.shapeMockup = function () {
+        var $shape = $(this);
+        $shape.each(function () {
+            var $currentShape = $(this),
+                shapeTop = $currentShape.data("top"),
+                shapeRight = $currentShape.data("right"),
+                shapeBottom = $currentShape.data("bottom"),
+                shapeLeft = $currentShape.data("left");
+            $currentShape
+                .css({
+                    top: shapeTop,
+                    right: shapeRight,
+                    bottom: shapeBottom,
+                    left: shapeLeft,
+                })
+                .removeAttr("data-top")
+                .removeAttr("data-right")
+                .removeAttr("data-bottom")
+                .removeAttr("data-left")
+                .parent()
+                .addClass("shape-mockup-wrap");
+        });
+    };
+
+    if ($(".shape-mockup")) {
+        $(".shape-mockup").shapeMockup();
+    }
+
+    /*----------- 14. Progress Bar Animation ----------*/
+    $('.progress-bar').waypoint(function() {
+        $('.progress-bar').css({
+        animation: "animate-positive 2.6s",
+        opacity: "1"
+        });
+    }, { offset: '75%' });
+    
+    // /*----------- 00. Right Click Disable ----------*/
+    //   window.addEventListener('contextmenu', function (e) {
+    //     // do something here...
+    //     e.preventDefault();
+    //   }, false);
+
+    // /*----------- 00. Inspect Element Disable ----------*/
+    //   document.onkeydown = function (e) {
+    //     if (event.keyCode == 123) {
+    //       return false;
+    //     }
+    //     if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+    //       return false;
+    //     }
+    //     if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+    //       return false;
+    //     }
+    //     if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+    //       return false;
+    //     }
+    //     if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+    //       return false;
+    //     }
+    // }
+    
+})(jQuery);
+
+
+
+
+
+
+
+
